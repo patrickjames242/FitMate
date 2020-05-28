a3b8cce6a89128dd60462bf49cc3b012c04cc77d
Patrick Hanna <patrickjh1998@hotmail.com>
2020-05-28 19:11:51 -0400

    initial

---
+++ FitLive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitLive
+//
+//  Created by Patrick Hanna on 5/28/20.
+//  Copyright © 2020 Patrick Hanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
