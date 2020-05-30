b8081fa4b992d1894ab00325383d6a85b823338b
Akeil Smith <asmithsrs04@gmail.com>
2020-05-30 03:38:34 -0400

    correspoding view controllers + home screen view controller add

---
+++ FitLive/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  FitLive
+//
+//  Created by Akeil S on 5/30/20.
+//  Copyright © 2020 Patrick Hanna. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
