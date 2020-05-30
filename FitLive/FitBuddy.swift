7696fc27cacd5ad13cd767a2b0a3b523d1535288
Akeil Smith <asmithsrs04@gmail.com>
2020-05-30 03:55:54 -0400

    Segues added, fit buddy page template added

---
+++ FitLive/FitBuddy.swift
@@ -0,0 +1,15 @@
+//
+//  FitBuddy.swift
+//  FitLive
+//
+//  Created by Akeil S on 5/30/20.
+//  Copyright © 2020 Patrick Hanna. All rights reserved.
+//
+
+import Foundation
+
+struct UserInfo {
+    let name: String
+    let username: String
+    let points: Int
+}
